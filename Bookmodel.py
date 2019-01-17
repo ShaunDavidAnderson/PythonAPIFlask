495362929a1845716082b1fb6e4aa28297a4071e
Shaun Anderson <shaundavidanderson@outlook.com>
2019-01-17 23:14:15 +0200

    Implementing and testing update price and name

--- Bookmodel.py
+++ Bookmodel.py
@@ -26,6 +26,16 @@ class Book(db.Model):
     def delete_book(_isbn):
         Book.query.filter_by(isbn=_isbn).delete()
         db.session.commit()
+
+    def update_book_price(_isbn, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        db.session.commit()
+    
+    def update_book_name(_isbn, _name):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        db.session.commit()
     
     
     def __repr__(self):
